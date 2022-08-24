03e7686a3a51f67e1d212bbe667886f4565c47c0
Krisna <krisnaerlangga08@gmail.com>
2022-08-24 20:34:04 +0700

    Get manny data

--- lib/http_page.dart
+++ lib/http_page.dart
@@ -14,9 +14,112 @@ class HttpPage extends StatefulWidget {
 class _HttpPageState extends State<HttpPage> {
   HttpModel? postResult;
   User? userResult;
+  String output = "no data";
 
   @override
   Widget build(BuildContext context) {
+    Widget buttonGet() {
+      return SizedBox(
+        width: double.infinity,
+        child: ElevatedButton(
+          style: ButtonStyle(
+            elevation: MaterialStateProperty.all(0),
+            padding: MaterialStateProperty.all(
+              const EdgeInsets.symmetric(vertical: 10),
+            ),
+            shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            backgroundColor: MaterialStateProperty.all(Colors.blue),
+          ),
+          onPressed: () {
+            User.connectToApi("2").then((value) {
+              userResult = value;
+              setState(() {});
+            });
+          },
+          child: Text(
+            "GET",
+            style: GoogleFonts.poppins(
+              fontWeight: FontWeight.w500,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      );
+    }
+
+    Widget buttonGetAll() {
+      return SizedBox(
+        width: double.infinity,
+        child: ElevatedButton(
+          style: ButtonStyle(
+            elevation: MaterialStateProperty.all(0),
+            padding: MaterialStateProperty.all(
+              const EdgeInsets.symmetric(vertical: 10),
+            ),
+            shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            backgroundColor: MaterialStateProperty.all(Colors.blue),
+          ),
+          onPressed: () {
+            User.getUsers("2").then((value) {
+              output = "";
+              for (var i = 0; i < value.length; i++) {
+                output = "$output[${value[i].firstName} ${value[i].lastName}]";
+                setState(() {});
+              }
+            });
+          },
+          child: Text(
+            "GET ALL",
+            style: GoogleFonts.poppins(
+              fontWeight: FontWeight.w500,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      );
+    }
+
+    Widget buttonPost() {
+      return SizedBox(
+        width: double.infinity,
+        child: ElevatedButton(
+          style: ButtonStyle(
+            elevation: MaterialStateProperty.all(0),
+            padding: MaterialStateProperty.all(
+              const EdgeInsets.symmetric(vertical: 10),
+            ),
+            shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            backgroundColor: MaterialStateProperty.all(Colors.blue),
+          ),
+          onPressed: () {
+            HttpModel.connectToApi("Krisna", "Presiden").then((value) {
+              postResult = value;
+              setState(() {});
+            });
+          },
+          child: Text(
+            "POST",
+            style: GoogleFonts.poppins(
+              fontWeight: FontWeight.w500,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      );
+    }
+
     return SafeArea(
         child: Scaffold(
       appBar: AppBar(
@@ -37,8 +140,8 @@ class _HttpPageState extends State<HttpPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                (postResult != null)
-                    ? "Post:\n${postResult!.id} - ${postResult!.name} - ${postResult!.job} - ${postResult!.createdAt}"
+                (userResult != null)
+                    ? "Get:\n${userResult!.id} - ${userResult!.firstName} - ${userResult!.lastName} - ${userResult!.email}"
                     : "Tidak ada data",
                 style: GoogleFonts.poppins(
                   fontWeight: FontWeight.w400,
@@ -49,8 +152,25 @@ class _HttpPageState extends State<HttpPage> {
                 height: 20,
               ),
               Text(
-                (userResult != null)
-                    ? "Post:\n${userResult!.id} - ${userResult!.firstName} - ${userResult!.lastName} - ${userResult!.email}"
+                "Get All:",
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 12,
+                ),
+              ),
+              Text(
+                output,
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 12,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                (postResult != null)
+                    ? "Post:\n${postResult!.id} - ${postResult!.name} - ${postResult!.job} - ${postResult!.createdAt}"
                     : "Tidak ada data",
                 style: GoogleFonts.poppins(
                   fontWeight: FontWeight.w400,
@@ -60,66 +180,15 @@ class _HttpPageState extends State<HttpPage> {
               const SizedBox(
                 height: 30,
               ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    elevation: MaterialStateProperty.all(0),
-                    padding: MaterialStateProperty.all(
-                      const EdgeInsets.symmetric(vertical: 10),
-                    ),
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    backgroundColor: MaterialStateProperty.all(Colors.blue),
-                  ),
-                  onPressed: () {
-                    User.connectToApi("2").then((value) {
-                      userResult = value;
-                      setState(() {});
-                    });
-                  },
-                  child: Text(
-                    "GET",
-                    style: GoogleFonts.poppins(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
+              buttonGet(),
+              const SizedBox(
+                height: 10,
               ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    elevation: MaterialStateProperty.all(0),
-                    padding: MaterialStateProperty.all(
-                      const EdgeInsets.symmetric(vertical: 10),
-                    ),
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    backgroundColor: MaterialStateProperty.all(Colors.blue),
-                  ),
-                  onPressed: () {
-                    HttpModel.connectToApi("Krisna", "Presiden").then((value) {
-                      postResult = value;
-                      setState(() {});
-                    });
-                  },
-                  child: Text(
-                    "POST",
-                    style: GoogleFonts.poppins(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
+              buttonGetAll(),
+              const SizedBox(
+                height: 10,
               ),
+              buttonPost(),
             ],
           ),
         ),
--- lib/user_model.dart
+++ lib/user_model.dart
@@ -34,4 +34,18 @@ class User {
     var userData = (jsonObject as Map<String, dynamic>)["data"];
     return User.createUser(userData);
   }
+
+  static Future<List<User>> getUsers(String page) async {
+    String apiUrl = "https://reqres.in/api/users?page=$page";
+    var apiResult = await http.get(Uri.parse(apiUrl));
+    var jsonObject = json.decode(apiResult.body);
+
+    List<dynamic> listUser = (jsonObject as Map<String, dynamic>)["data"];
+    List<User> users = [];
+    for (var i = 0; i < listUser.length; i++) {
+      users.add(User.createUser(listUser[i]));
+    }
+
+    return users;
+  }
 }
